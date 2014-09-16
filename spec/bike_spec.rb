a530bc9b66b51aace02f748b7c8c08759512b246
mala23 <mail@mala23.me>
2014-09-16 14:13:55 +0100

    Refactored the code

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -5,21 +5,20 @@ describe Bike do
 	# this is a specific feature (behaviour) 
 	# that we expect to be present
 
+	let(:bike) { Bike.new }
+
 	it "should not be broken after we create it" do
- 		the_bike = Bike.new # initialise a new object of Bike class
  		# expect an instance of this class to have 
  		# a method "broken?" that should return false
- 		expect(the_bike).not_to be_broken
+ 		expect(bike).not_to be_broken
 	end
 
  	it "should be able to break" do
-		bike = Bike.new
 		bike.break!
 		expect(bike).to be_broken
 	end
 
-	it "should be able to break" do
-		bike = Bike.new
+	it "should be able to get fixed" do
 		bike.break!
 		bike.fix!
 		expect(bike).not_to be_broken
