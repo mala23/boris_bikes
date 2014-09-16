9c1cf4b9cb6aa36b2e39a2edd2c2a39a3cb38082
mala23 <mail@mala23.me>
2014-09-16 19:38:47 +0100

    Refactoring

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,46 +1,10 @@
-require './lib/docking_station.rb'
+require './lib/docking_station'
 
 describe DockingStation do
 
-	def fill_station(station)
-		20.times { station.dock(Bike.new) }
-	end
+  let(:station) { DockingStation.new(:capacity => 123) }
 
-	let(:bike) { Bike.new }
-	let(:station) { DockingStation.new(:capacity => 20) }
-	
-	it 'should accept a bike' do
-		# we expect the station to have 0 bikes
-		expect(station.bike_count).to eq(0)
-		# let's dock a bike into the station
-		station.dock(bike)
-		# now we expect the station to have 1 bike
-		expect(station.bike_count).to eq(1)
-	end
-
-	it 'should release a bike' do
-		station.dock(bike)
-		station.release(bike)
-		expect(station.bike_count).to eq(0)
-	end
-
-	it "should know when it's full" do
-		expect(station).not_to be_full
-		20.times { station.dock(Bike.new) }
-		expect(station).to be_full
-	end
-
-	it "should not accept a bike if it's full" do
-		fill_station station
-		expect(lambda { station.dock(bike) }).to raise_error(RuntimeError)
-	end
-
-	it "should provide the list of available bikes" do
-		working_bike, broken_bike = Bike.new, Bike.new
-		broken_bike.break!
-		station.dock(working_bike)
-		station.dock(broken_bike)
-		expect(station.available_bikes).to eq([working_bike])
-	end
-
+  it "should allow setting default capacity on initialising" do
+    expect(station.capacity).to eq(123)
+  end
 end
